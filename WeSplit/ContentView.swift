7d58740db7694674579bd100c085dbfe2f0b2a6f
Frank Guchelaar <fguchelaar@gmail.com>
2019-10-11 15:32:50 +0200

    Initial Commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Frank Guchelaar on 11/10/2019.
+//  Copyright © 2019 Awesomation. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
