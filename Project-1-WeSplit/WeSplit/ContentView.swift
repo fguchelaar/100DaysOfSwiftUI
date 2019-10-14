a83e8135be7af909d709d89e8613d67403a2c8b6
Frank Guchelaar <fguchelaar@gmail.com>
2019-10-14 21:21:43 +0200

    Finished project 1: WeSplit.

---
+++ Project-1-WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,65 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Frank Guchelaar on 11/10/2019.
+//  Copyright © 2019 Awesomation. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 2
+
+    let tipPercentages = [10, 15, 20, 25, 0]
+
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentages[tipPercentage])
+        let amount = Double(checkAmount) ?? 0.0
+
+        return (amount + (amount * tipSelection / 100)) / peopleCount
+    }
+
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", text: $checkAmount)
+                        .keyboardType(.decimalPad)
+
+                    Picker("Number of people", selection: $numberOfPeople) {
+
+                        ForEach(2 ..< 100) {
+                            Text("\($0) people")
+                        }
+                    }
+                    .pickerStyle(DefaultPickerStyle())
+                }
+
+                Section(header: Text("How much do you want to tip?")) {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(0..<tipPercentages.count) {
+                            Text("\(self.tipPercentages[$0])%")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+
+                Section(header: Text("Amount per person")) {
+                    Text("$\(totalPerPerson, specifier: "%.2f")")
+                }
+            }
+            .navigationBarTitle("WeSplit")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
