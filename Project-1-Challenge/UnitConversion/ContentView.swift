4c6d0953c7283592b991d4a2d3d8ab94827fffa7
Frank Guchelaar <fguchelaar@gmail.com>
2019-10-14 21:48:30 +0200

    Completed the challenge after project 1.
    Decided to use the `Measurement` api to do the conversion.

--- Project-1-Challenge/UnitConversion/ContentView.swift
+++ Project-1-Challenge/UnitConversion/ContentView.swift
@@ -9,8 +9,55 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    @State private var selectedFromType = 0
+    @State private var selectedToType = 0
+    @State private var input = "0"
+
+    private var output: Double {
+        let inputValue = Double(input) ?? 0;
+
+        let measurement = Measurement(value: inputValue, unit: unitTypes[selectedFromType].1)
+
+        return measurement.converted(to: unitTypes[selectedToType].1).value
+    }
+
+    private let unitTypes = [
+        ("meters", UnitLength.meters),
+        ("kilometers", UnitLength.kilometers),
+        ("feet", UnitLength.feet),
+        ("yard", UnitLength.yards),
+        ("miles", UnitLength.miles)
+    ]
+
     var body: some View {
-        Text("Hello World")
+        NavigationView {
+            Form {
+                Section(header: Text("From")) {
+                    TextField("Input value", text: $input).keyboardType(.decimalPad)
+
+                    Picker("From units", selection: $selectedFromType) {
+                        ForEach(0..<unitTypes.count) {
+                            Text("\(self.unitTypes[$0].0)")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+
+                Section(header: Text("To")) {
+                    Picker("To units", selection: $selectedToType) {
+                        ForEach(0..<unitTypes.count) {
+                            Text("\(self.unitTypes[$0].0)")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+
+                    Text("\(output, specifier: "%.2f")")
+                }
+
+            }
+            .navigationBarTitle("Unit Conversion")
+        }
     }
 }
 
