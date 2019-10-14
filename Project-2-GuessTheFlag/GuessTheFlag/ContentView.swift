beb4f3375481005012e4a72115a6a1a1fc3ff2e2
Frank Guchelaar <fguchelaar@gmail.com>
2019-10-14 23:08:26 +0200

    Created Xcode project for _Project 2_.

---
+++ Project-2-GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by Frank Guchelaar on 14/10/2019.
+//  Copyright © 2019 Awesomation. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
