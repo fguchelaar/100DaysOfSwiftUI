6706d514b7b551bb1b65b809c7d7620d9ed2a912
Frank Guchelaar <fguchelaar@gmail.com>
2019-11-01 15:43:55 +0100

    project number 8

---
+++ Project-08-Moonshot/Moonshot/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Moonshot
+//
+//  Created by Frank Guchelaar on 01/11/2019.
+//  Copyright © 2019 Awesomation. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        VStack {
+            Image("Monaco")
+        }
+    }
+}
